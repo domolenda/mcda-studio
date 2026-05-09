0b24ad4ac7fa168d7c6b5327e3ad9042afef1874
Dominik Molenda <molenda.dominik@yahoo.com>
2026-05-09 21:10:00 +0200

    fix(backend): update pydantic settings to use SettingsConfigDict

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     app_title: str
     app_version: str
     frontend_origin: str
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()  # type: ignore
