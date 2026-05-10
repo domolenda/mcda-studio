8259168cc9427c42d885c867c573179300e127ed
Dominik Molenda <molenda.dominik@yahoo.com>
2026-05-10 13:27:27 +0200

    chore(frontend): init vue project with primevue and tailwind

---
+++ frontend/src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import './assets/main.css'
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark',
+    },
+  },
+})
+
+app.mount('#app')
