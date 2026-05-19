1874ee0f8ba5f4b74412468bc8cbddd535c2dc84
Dominik Molenda <molenda.dominik@yahoo.com>
2026-05-19 14:35:09 +0200

    feat(frontend): init results view

--- frontend/src/views/ResultsView.vue
+++ frontend/src/views/ResultsView.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>ResultsView</div>
+  <div v-if="dataStore.data && resultsStore.results"></div>
+  <div v-else class="text-center p-8 text-surface-500">
+    No data available. Please run analysis first.
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useDataStore } from '@/stores/dataStore'
+import { useResultsStore } from '@/stores/resultsStore'
+
+const dataStore = useDataStore()
+const resultsStore = useResultsStore()
+</script>
